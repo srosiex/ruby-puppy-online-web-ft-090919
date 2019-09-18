5c9cbdc7f7cfc4bf3de18011586fe400b89081e7
Sarah H <sarahrose.henderson@gmail.com>
2019-09-18 22:19:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,21 @@
 class Dog
   @@all = []
+  @@clear_all = []
   attr_accessor :name
 
   def initialize(name)
     @name = name
     @@all << self
+    @@clear_all << @@all
   end
 
   def self.all
     @@all
   end
+
+  def self.clear_all
+@@clear_all.clear
+  end
 end
+
+
